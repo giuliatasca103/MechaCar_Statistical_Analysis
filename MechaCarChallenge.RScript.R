ea6fae0a1c3e9c2ed5af866300eaa2580319222e
Giulia Tasca <giuliatasca@Giulias-MacBook-Pro.local>
2021-07-14 13:02:33 -0700

    adding files

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -7,3 +7,11 @@ MechaCar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 fit1 <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, MechaCar)
 
 summary(fit1)
+
+suspension <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+total_summary <- suspension %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),Variance_PSI=var(PSI),SD_PSI=sd(PSI), .groups = 'keep')
+total_summary
+
+lot_summary <- suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),Variance_PSI=var(PSI),SD_PSI=sd(PSI), .groups = 'keep')
+lot_summary
